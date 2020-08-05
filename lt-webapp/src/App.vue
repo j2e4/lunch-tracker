589e8c6fc39320bd38b018324c47ecba6c11ae2d
김정은 <jeongeun.kim@innowireless.com>
2020-08-05 17:54:49 +0900

    [#4] 틀 잡기
    * FontAwesome, Bootstrap 라이브러리를 추가했다.
    * 메인으로 뜨는 화면을 Vue Home화면에서 LT Main 화면으로 수정했다.

--- lt-webapp/src/App.vue
+++ lt-webapp/src/App.vue
@@ -1,32 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Main</router-link>
+            |
+            <router-link to="/home">Vue</router-link>
+        </div>
+        <router-view/>
     </div>
-    <router-view />
-  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    #app {
+        // TODO 공용으로 빼기
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        display: flex;
+        flex-flow: column;
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
     }
-  }
-}
+
+    #nav {
+        padding: 30px;
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
 </style>
---
+++ lt-webapp/src/views/Main.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="lt-main">
+        <div class="lt-main-content-area">
+
+        </div>
+        <BNav
+            class="lt-main-nav"
+            vertical
+        >
+            <BNavItem>
+                <FontAwesomeIcon icon="home" />
+            </BNavItem>
+        </BNav>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "Main"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .lt-main {
+        display: flex;
+        flex-flow: row nowrap;
+        flex: 1 0 auto;
+        position: relative;
+
+        > .lt-main-content-area {
+            flex: 1 0 auto;
+        }
+
+        > .lt-main-nav {
+            margin: auto 0;
+            list-style: none;
+            position: relative;
+
+            a {
+                color: #2c3e50;
+                margin-right: 5px;
+            }
+        }
+    }
+</style>
